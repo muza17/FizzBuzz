026a3c5eedb40faefcd63f7bb6c5fefef859d8a1
muza17 <inomovamahfuza404@gmail.com>
2021-11-14 14:11:44 +0300

    add minor

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var form = document.getElementById("form");
+var input = document.getElementById("input");
+var result = document.getElementById("result");
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    if(input.value% 5 == 0 && input.value % 3 == 0){
+        result.textContent = "fizzbuzz";
+    } else if(input.value % 5 == 0){
+        result.textContent = "fizz";
+    }else if(input.value % 3 == 0){
+        result.textContent = "buzz";
+    } else{
+        result.textContent = "Qayta urining"; 
+    }
+    input.value = "";
+})
